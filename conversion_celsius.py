8f422ef11db47f70e199966657830f01c4debe5f
sergio <pardosh88@gmai.com>
2019-04-26 11:52:14 +0200

    ejercicio para convertir de grados a celsius

--- conversion_celsius.py
+++ conversion_celsius.py
@@ -1,11 +1,6 @@
 
 print("Bienvenido al conversor de Celsius a Farenheit")
 
-celsius = (input("Indícame en números los grados celisus:\n"))
+celsius = float(input("Indícame en números los grados celisus:\n"))
 
-#while celsius is not int
-#    celsius = (input("Indícame en NÚMEROS los grados celisus:\n"))
-
-farenheit = (int(celsius) * 9 / 5) + 32
-
-print("{} grados Celsius equivalen a {} grados Farenheit".format(celsius, farenheit))
+print(f"{celsius} grados Celsius equivalen a {round(celsius * 9 / 5 + 32)} grados Farenheit")
